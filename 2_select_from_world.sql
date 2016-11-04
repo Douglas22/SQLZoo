1c410c0d55e0793d8f55535a5992b2291da422dc
Douglas <Douglas22@users.noreply.github.com>
2016-11-04 11:35:55 +0000

    Update 2_select_from_world.sql

--- 2_select_from_world.sql
+++ 2_select_from_world.sql
@@ -8,18 +8,18 @@ FROM world;
 -- # zeros)
 SELECT name
 FROM world
-WHERE population > 200000000;
+WHERE population > 2*power(10,8);
 
 -- # 3. Give the name and the per capita GDP for those countries with a
 -- # population of at least 200 million.
 SELECT name, gdp/population
 FROM world
-WHERE population > 200000000;
+WHERE population > 2*power(10,8);
 
 -- # 4. Show the name and population in millions for the countries of
 -- # 'South America' Divide the population by 1000000 to get population
 -- # in millions.
-SELECT name, population/1000000
+SELECT name, population/power(10,6)
 FROM world
 WHERE continent = 'South America';
 
